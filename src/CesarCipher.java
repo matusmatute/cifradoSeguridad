1aabf0b5f33cf155b9e10f0b44c7b2e7a4dd1d01
matusmatute <laura.matus99@gmail.com>
2024-02-21 18:25:55 -0600

    reere

--- src/CesarCipher.java
+++ src/CesarCipher.java
@@ -5,6 +5,8 @@ public class CesarCipher {
     public static String cifrar(String mensaje, int desplazamiento) {
         StringBuilder resultado = new StringBuilder();
 
+        System.out.println("\nMatriz de cifrado:");
+        System.out.println("Texto Claro\tCriptograma");
         for (int i = 0; i < mensaje.length(); i++) {
             char caracter = mensaje.charAt(i);
 
@@ -20,14 +22,21 @@ public class CesarCipher {
                 codigoAscii += desplazamiento;
 
                 // Ajustar el código ASCII si se sale del rango de letras
-                if (esMayuscula && codigoAscii > 'Z' || !esMayuscula && codigoAscii > 'z') {
-                    codigoAscii -= 26;
-                } else if (esMayuscula && codigoAscii < 'A' || !esMayuscula && codigoAscii < 'a') {
-                    codigoAscii += 26;
+                if (esMayuscula) {
+                    while (codigoAscii > 'Z') {
+                        codigoAscii -= 26;
+                    }
+                } else {
+                    while (codigoAscii > 'z') {
+                        codigoAscii -= 26;
+                    }
                 }
 
-                // Agregar el caracter cifrado al resultado
-                resultado.append((char) codigoAscii);
+                char criptogramaChar = (char) codigoAscii;
+                resultado.append(criptogramaChar);
+
+                // Imprimir la matriz de cifrado
+                System.out.println(caracter + "\t\t" + criptogramaChar);
             } else {
                 // Mantener los caracteres que no son letras sin modificar
                 resultado.append(caracter);
@@ -39,8 +48,47 @@ public class CesarCipher {
 
     // Método para desencriptar un mensaje cifrado utilizando el cifrado César con un desplazamiento dado
     public static String descifrar(String mensajeCifrado, int desplazamiento) {
-        // Para descifrar, simplemente se utiliza el cifrado con un desplazamiento negativo
-        return cifrar(mensajeCifrado, -desplazamiento);
+        StringBuilder resultado = new StringBuilder();
+
+        System.out.println("\nMatriz de descifrado:");
+        System.out.println("Criptograma\tTexto Claro");
+        for (int i = 0; i < mensajeCifrado.length(); i++) {
+            char caracter = mensajeCifrado.charAt(i);
+
+            // Verificar si el caracter es una letra
+            if (Character.isLetter(caracter)) {
+                // Obtener el código ASCII del caracter
+                int codigoAscii = (int) caracter;
+
+                // Determinar si el caracter es mayúscula o minúscula
+                boolean esMayuscula = Character.isUpperCase(caracter);
+
+                // Aplicar el desplazamiento
+                codigoAscii -= desplazamiento;
+
+                // Ajustar el código ASCII si se sale del rango de letras
+                if (esMayuscula) {
+                    while (codigoAscii < 'A') {
+                        codigoAscii += 26;
+                    }
+                } else {
+                    while (codigoAscii < 'a') {
+                        codigoAscii += 26;
+                    }
+                }
+
+                char textoClaroChar = (char) codigoAscii;
+                resultado.append(textoClaroChar);
+
+                // Imprimir la matriz de descifrado
+                System.out.println(caracter + "\t\t" + textoClaroChar);
+            } else {
+                // Mantener los caracteres que no son letras sin modificar
+                resultado.append(caracter);
+            }
+        }
+
+        return resultado.toString();
     }
 
     public static void main(String[] args) {
@@ -57,8 +105,7 @@ public class CesarCipher {
         String criptograma = cifrar(palabra, desplazamiento);
 
         // Mostrar el resultado del cifrado
-        System.out.println("Texto Claro: " + palabra);
-        System.out.println("Criptograma: " + criptograma);
+        System.out.println("\nCriptograma: " + criptograma);
 
         // Solicitar al usuario que ingrese el criptograma a descifrar
         System.out.print("\nIngrese el criptograma a descifrar: ");
@@ -68,9 +115,8 @@ public class CesarCipher {
         String mensajeDescifrado = descifrar(criptogramaIngresado, desplazamiento);
 
         // Mostrar el resultado del descifrado
-        System.out.println("Mensaje Descifrado: " + mensajeDescifrado);
+        System.out.println("\nMensaje Descifrado: " + mensajeDescifrado);
 
         scanner.close();
     }
 }
-
